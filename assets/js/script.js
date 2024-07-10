c9679255cef0eac6e8d42e80c0e88f21bb94f31d
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-07-10 12:29:42 -0700

    add few files and code of nested loop

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// nested loop programs
+
+//create a prgram about tringles using numbers
+
+// for (var a = 1; a <= 5; a++){
+//     for (var b = 1; b <= a; b++){
+//         document.write(b + " ")
+       
+//     }
+//     document.write("<br>")
+// }
+
+//create a prgram about tringles using numbers
+
+// for (var a = 1; a <= 5; a++){
+//     for (var b = 1; b <= a; b++){
+//         document.write(a + " | ")
+       
+//     }
+//     document.write("<br>")
+// }
