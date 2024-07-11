fee3f9cf5aac03f070cf66dd82feff46b44d62d1
Mohib-Digital <mohibullahafridi2020@gmail.com>
2024-07-11 14:19:48 -0700

    add for loops and nested for loops codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,113 @@
-// nested loop programs
+//Chapter No.19 (for loops: Flags, Booleans, array length, and loopus interruptus) 
 
-//create a prgram about tringles using numbers
+// var cleanestCities = ["Peshawar", "Islamabad", "Lahore", "Multan", "Karachi"]
+// var enterYourCity = prompt("Please! Enter your city name:")
 
-// for (var a = 1; a <= 5; a++){
-//     for (var b = 1; b <= a; b++){
-//         document.write(b + " ")
-       
+// flag = false;
+
+// for (var i = 0; i < cleanestCities.length; i++) {
+//     if (enterYourCity === cleanestCities[i]) {
+//         flag = true;
+//         // loopus interruptus
+//         break;
 //     }
-//     document.write("<br>")
+//     // console.log(i)
 // }
 
-//create a prgram about tringles using numbers
-
-// for (var a = 1; a <= 5; a++){
-//     for (var b = 1; b <= a; b++){
-//         document.write(a + " | ")
-       
-//     }
-//     document.write("<br>")
+// if (flag) {
+//     console.log(`${enterYourCity} is one of the cleanest city`)
+// }else {
+//     console.log("Sorry! Not in the list.")
 // }
+
+// or 
+
+// if (enterYourCity === true) {
+//     console.log(`${enterYourCity} is one of the cleanest city`)
+// }else {
+//     console.log("Sorry! Not in the list.")
+// }
+// ==================================================================================
+
+// Chapter No.20   (for loops nested)
+
+// var incremented = 0;
+
+// for (var i = 1; i <= 3; i++) {
+//      for (var j = 1; j <= 3; j++) {
+//         console.log(`i: ${i}, j: ${j}`)
+//         // incremented++;
+//     }
+// }
+// console.log({incremented})
+// ==================================================================================
+
+// multiplication of table (5x5) 
+// for (var i = 1; i <= 5; i++) {
+//     for (var j = 1; j <= 5; j++) {
+//        console.log(`${i} * ${j}= ${i * j}`)
+
+//    }
+// }
+// ===================================================================================
+
+// make any multiplication table
+
+// var num = +prompt("Please enter any number!")
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(`${num} * ${i} = ${num * i}`)
+// }
+// ===================================================================================
+
+
+// triangle pattern with stars(*)
+// for (i = 1; i <= 5; i++) {
+//     var stars = "";
+//     for (j = 1; j <= i; j++) {
+//         stars += "*";
+//     }
+//     console.log(stars)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
